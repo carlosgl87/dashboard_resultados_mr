458e796ff706777c99a7b819b77933c32ee312d2
Carlos Gamero <cgamero@unique-yanbal.com>
2022-10-15 10:38:36 -0500

    multipages

--- app.py
+++ app.py
@@ -97,119 +97,102 @@ dfTextExtract = dfTextExtract.join(df)
 #### UI Streamlit ####
 ######################
 
-## SUMMARY TABLE
-st.header('SUMMARY')
+def main_page():
+  ## SUMMARY TABLE
+  st.header('SUMMARY')
 
-df_temp = dfTags.groupby('nameCase').agg({'tagID':'nunique','fileName':'nunique'}).reset_index()
-df_temp = df_temp.rename(columns={'nameCase': 'Case Name','fileName':'Number Documents','tagID':'Number Tags'})
-df_temp = df_temp[['Case Name','Number Documents','Number Tags']]
+  df_temp = dfTags.groupby('nameCase').agg({'tagID':'nunique','fileName':'nunique'}).reset_index()
+  df_temp = df_temp.rename(columns={'nameCase': 'Case Name','fileName':'Number Documents','tagID':'Number Tags'})
+  df_temp = df_temp[['Case Name','Number Documents','Number Tags']]
 
-st.dataframe(data=df_temp)
+  st.dataframe(data=df_temp)
 
-## DETAILS
-st.header('DETAILS')
+  ## DETAILS
+  st.header('DETAILS')
 
-documento = st.selectbox(
-    '¿What Case do you want to select?',
-    [x[:-4] for x in list(df_temp['Case Name'].unique())if x not in '9781234 (1).pdf'])
+  documento = st.selectbox(
+      '¿What Case do you want to select?',
+      [x[:-4] for x in list(df_temp['Case Name'].unique())if x not in '9781234 (1).pdf'])
 
-## DISPLAY EACH SECTION
+  ## DISPLAY EACH SECTION
 
-dfTagsSelection = dfTags[dfTags['nameCase']=='Cari_Woodford']
-
-
-for caseTitle in list(dfTagsSelection['tagTitle'].unique()):
-  st.subheader(caseTitle)
-  for text in list(dfTagsSelection[dfTagsSelection['tagTitle'] == caseTitle]['text'].unique()):
-    st.markdown(text)
-    temp = dfTagsSelection[dfTagsSelection['text'] == text]
-    df_interseccion = pd.DataFrame(columns=['Document','Page','Text','Position'])
-    cont = 0
-    for i in list(temp.index):
-      documento = temp.loc[i]['fileName']
-      pagina = temp.loc[i]['page']
-      TotalY = temp['pageHeight'].loc[i]
-      TotalX = temp['pageWidth'].loc[i]
-      tag_eval = posicionTag(dfTagsSelection,i,TotalX,TotalY)
-      dfTextExtractSelection = dfTextExtract[dfTextExtract['Documento']==documento[:-4]].reset_index(drop=True)
-      print(tag_eval)
-      text_extract = ''
-      for j in list(dfTextExtractSelection[dfTextExtractSelection['Page']==pagina].index):
-        if isRectangleOverlap(tag_eval,posicionAWS(j,dfTextExtractSelection)):
-          text_extract = text_extract + dfTextExtractSelection.loc[j]['Text']
-      #print(text_extract)
-      df_interseccion.loc[cont] = [documento,pagina,text_extract,tag_eval]
-      cont = cont + 1
-      #print(text_extract)
-    st.dataframe(data=df_interseccion)
-  #  print(df_interseccion)
-
-#dfTagsSelection = dfTags[dfTags['fileName']=='Caso_2_1652446328707-1.pdf']
-#for text in dfTagsSelection[dfTagsSelection['tagTitle']=='Family History']['text'].unique():
-#  text = "- " + text 
-#  st.markdown(text)
+  dfTagsSelection = dfTags[dfTags['nameCase']=='Cari_Woodford']
 
 
+  for caseTitle in list(dfTagsSelection['tagTitle'].unique()):
+    st.subheader(caseTitle)
+    for text in list(dfTagsSelection[dfTagsSelection['tagTitle'] == caseTitle]['text'].unique()):
+      st.markdown(text)
+      temp = dfTagsSelection[dfTagsSelection['text'] == text]
+      df_interseccion = pd.DataFrame(columns=['Document','Page','Text','Position'])
+      cont = 0
+      for i in list(temp.index):
+        documento = temp.loc[i]['fileName']
+        pagina = temp.loc[i]['page']
+        TotalY = temp['pageHeight'].loc[i]
+        TotalX = temp['pageWidth'].loc[i]
+        tag_eval = posicionTag(dfTagsSelection,i,TotalX,TotalY)
+        dfTextExtractSelection = dfTextExtract[dfTextExtract['Documento']==documento[:-4]].reset_index(drop=True)
+        print(tag_eval)
+        text_extract = ''
+        for j in list(dfTextExtractSelection[dfTextExtractSelection['Page']==pagina].index):
+          if isRectangleOverlap(tag_eval,posicionAWS(j,dfTextExtractSelection)):
+            text_extract = text_extract + dfTextExtractSelection.loc[j]['Text']
+        #print(text_extract)
+        df_interseccion.loc[cont] = [documento,pagina,text_extract,tag_eval]
+        cont = cont + 1
+        #print(text_extract)
+      st.dataframe(data=df_interseccion)
+    #  print(df_interseccion)
 
-# st.subheader('Case Summary')
+  #dfTagsSelection = dfTags[dfTags['fileName']=='Caso_2_1652446328707-1.pdf']
+  #for text in dfTagsSelection[dfTagsSelection['tagTitle']=='Family History']['text'].unique():
+  #  text = "- " + text 
+  #  st.markdown(text)
+  # st.subheader('Case Summary')
+  # st.subheader('Key Studies and Interventions')
+  # st.subheader('History of Present Illness')
+  # st.subheader('Past Medical History')
+  # st.subheader('Past Surgical History')
+  # st.subheader('Social History - EtOH')
+  # st.subheader('Social History - Smoking Status')
+  # st.subheader('Social History - Illicit Substance Use')
+  # st.subheader('Family History')
+  # st.subheader('Allergies')
+  # st.subheader('Medications')
+  # df_interseccion = pd.DataFrame(columns=['summary_tag','summary_text','text_extract'])
+  # dfTagsSelection = dfTags[dfTags['fileName']==documento+'.pdf'].reset_index(drop=True)
+  # dfTextExtractSelection = dfTextExtract[dfTextExtract['Documento']==documento].reset_index(drop=True)
+  # cont = 0
+  # for i in range(len(dfTagsSelection)):
+  #   pagina = dfTagsSelection.loc[i]['page']
+  #   TotalY = dfTagsSelection['pageHeight'].loc[0]
+  #   TotalX = dfTagsSelection['pageWidth'].loc[0]
+  #   tag_eval = posicionTag(dfTagsSelection,i,TotalX,TotalY)
+  #   summary_tag = dfTagType[dfTagType['_id']==ObjectId(dfTagsSelection.loc[i]['tag']['tag'])].reset_index(drop=True)['tag'].loc[0]
+  #   summary_text = dfTagsSelection.loc[i]['tag']['text']
+  #   for j in list(dfTextExtractSelection[dfTextExtractSelection['Page']==pagina].index):
+  #     if isRectangleOverlap(tag_eval,posicionAWS(j,dfTextExtractSelection)):
+  #       text_extract = dfTextExtractSelection.loc[j]['Text']
+  #       df_interseccion.loc[cont] = [summary_tag,summary_text,text_extract]
+  #       cont = cont + 1
 
+  # st.dataframe(data=df_interseccion)
 
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
 
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
 
-# st.subheader('Key Studies and Interventions')
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
 
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
 
-
-
-# st.subheader('History of Present Illness')
-
-
-
-# st.subheader('Past Medical History')
-
-
-
-# st.subheader('Past Surgical History')
-
-
-
-# st.subheader('Social History - EtOH')
-
-
-# st.subheader('Social History - Smoking Status')
-
-
-# st.subheader('Social History - Illicit Substance Use')
-
-
-# st.subheader('Family History')
-
-
-# st.subheader('Allergies')
-
-
-
-# st.subheader('Medications')
-
-
-
-
-
-# df_interseccion = pd.DataFrame(columns=['summary_tag','summary_text','text_extract'])
-# dfTagsSelection = dfTags[dfTags['fileName']==documento+'.pdf'].reset_index(drop=True)
-# dfTextExtractSelection = dfTextExtract[dfTextExtract['Documento']==documento].reset_index(drop=True)
-# cont = 0
-# for i in range(len(dfTagsSelection)):
-#   pagina = dfTagsSelection.loc[i]['page']
-#   TotalY = dfTagsSelection['pageHeight'].loc[0]
-#   TotalX = dfTagsSelection['pageWidth'].loc[0]
-#   tag_eval = posicionTag(dfTagsSelection,i,TotalX,TotalY)
-#   summary_tag = dfTagType[dfTagType['_id']==ObjectId(dfTagsSelection.loc[i]['tag']['tag'])].reset_index(drop=True)['tag'].loc[0]
-#   summary_text = dfTagsSelection.loc[i]['tag']['text']
-#   for j in list(dfTextExtractSelection[dfTextExtractSelection['Page']==pagina].index):
-#     if isRectangleOverlap(tag_eval,posicionAWS(j,dfTextExtractSelection)):
-#       text_extract = dfTextExtractSelection.loc[j]['Text']
-#       df_interseccion.loc[cont] = [summary_tag,summary_text,text_extract]
-#       cont = cont + 1
-
-# st.dataframe(data=df_interseccion)
